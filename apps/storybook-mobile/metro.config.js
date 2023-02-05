8d0250bd340d301fd964ec0de3fd8cdf7a393185
Nikita Povalishev <np@skl.vc>
2023-02-05 21:24:58 +0300

    Create project for storybook mobile

---
+++ apps/storybook-mobile/metro.config.js
@@ -0,0 +1,42 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+const {
+  getMetroTools,
+  getMetroAndroidAssetsResolutionFix,
+} = require('react-native-monorepo-tools');
+
+const monorepoMetroTools = getMetroTools();
+const androidAssetsResolutionFix = getMetroAndroidAssetsResolutionFix();
+
+module.exports = {
+  transformer: {
+    publicPath: androidAssetsResolutionFix.publicPath,
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+
+  server: {
+    // Server middleware
+    enhanceMiddleware: (middleware) => androidAssetsResolutionFix.applyMiddleware(middleware),
+  },
+
+  // Add additional Yarn workspace package roots to the module map.
+  // This allows importing importing from all the project's packages.
+  watchFolders: monorepoMetroTools.watchFolders,
+  resolver: {
+    // Ensure we resolve nohoist libraries from this directory.
+    blockList: exclusionList(monorepoMetroTools.blockList),
+    extraNodeModules: monorepoMetroTools.extraNodeModules,
+  },
+};
